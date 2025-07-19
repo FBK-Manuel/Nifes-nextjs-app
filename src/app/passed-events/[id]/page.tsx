fbcce6b45ab0c59036ceef684a4c43f4884ba3eb
FBK-Manuel <emmanuelbigboss26@gmmail.com>
2025-07-19 18:41:40 +0100

    ids update

--- src/app/passed-events/[id]/page.tsx
+++ src/app/passed-events/[id]/page.tsx
@@ -1,52 +1,51 @@
-export const dynamic = "force-dynamic"; // 👈 this is crucial
+// import { doc, getDoc } from "firebase/firestore";
+// import { db } from "../../lib/firebase";
+// import Image from "next/image";
+// import { format } from "date-fns";
+// import { notFound } from "next/navigation";
+// // import { Props } from "../../types/datatypes";
+// import { TiArrowForward } from "react-icons/ti";
 
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../lib/firebase";
-import Image from "next/image";
-import { format } from "date-fns";
-import { notFound } from "next/navigation";
-import { TiArrowForward } from "react-icons/ti";
+export default async function PastedEventsPage() {
+// {
+// params,
+// }: { params: { id: string }; }
+  // const docRef = doc(db, "passedEvents", params.id);
+  // const docSnap = await getDoc(docRef);
 
-export default async function PastedEventsPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const docRef = doc(db, "passedEvents", params.id);
-  const docSnap = await getDoc(docRef);
+  // if (!docSnap.exists()) {
+  //   notFound();
+  // }
 
-  if (!docSnap.exists()) {
-    notFound();
-  }
-
-  const data = docSnap.data();
-  const movedAt = data.movedAt?.toDate() ?? new Date();
+  // const data = docSnap.data();
+  // const movedAt = data.movedAt?.toDate() ?? new Date();
 
   return (
-    <main className="max-w-3xl mx-auto px-4 py-8">
-      <div className="relative w-full h-64 md:h-96 lg:h-[700px] mb-6">
-        <Image
-          src={data.imageUrl}
-          alt={data.title}
-          fill
-          className="object-cover rounded-lg"
-        />
-      </div>
+    <></>
+    // <main className="max-w-3xl mx-auto px-4 py-8">
+    //   <div className="relative w-full h-64 md:h-96 lg:h-[700px] mb-6">
+    //     <Image
+    //       src={data.imageUrl}
+    //       alt={data.title}
+    //       fill
+    //       className="object-cover rounded-lg"
+    //     />
+    //   </div>
 
-      <p className="text-xs text-gray-400 mb-2">
-        Passed on {format(movedAt, "MMMM d, yyyy")}
-      </p>
-      <h1 className="text-2xl font-bold text-black mb-4">{data.title}</h1>
-      <p className="text-[#202650] whitespace-pre-line">{data.content}</p>
-      <div className="flex items-center justify-center mt-7 gap-4">
-        <button
-          disabled
-          className="inline-block bg-[#202650] text-white px-6 py-3 rounded-lg hover:bg-[#e5408f] hover:text-black delay-150 duration-300 ease-in-out transition"
-        >
-          Apply Now{" "}
-          <TiArrowForward className="inline ml-2 text-white hover:text-black text-3xl animate-pulse" />
-        </button>
-      </div>
-    </main>
+    //   <p className="text-xs text-gray-400 mb-2">
+    //     Passed on {format(movedAt, "MMMM d, yyyy")}
+    //   </p>
+    //   <h1 className="text-2xl font-bold text-black mb-4">{data.title}</h1>
+    //   <p className="text-[#202650] whitespace-pre-line">{data.content}</p>
+    //   <div className="flex items-center justify-center mt-7 gap-4">
+    //     <button
+    //       disabled
+    //       className="inline-block bg-[#202650] text-white px-6 py-3 rounded-lg hover:bg-[#e5408f] hover:text-black delay-150 duration-300 ease-in-out transition"
+    //     >
+    //       Apply Now{" "}
+    //       <TiArrowForward className="inline ml-2 text-white hover:text-black text-3xl animate-pulse" />
+    //     </button>
+    //   </div>
+    // </main>
   );
 }
